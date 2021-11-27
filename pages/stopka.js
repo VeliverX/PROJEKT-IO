2442955245479186be61b5d196cff9a6a2bbc577
Jakub Pszczółka <jp057198@student.ath.edu.pl>
2021-11-27 02:32:11 +0100

    Stopka

---
+++ pages/stopka.js
@@ -0,0 +1,94 @@
+import styles from 'tailwindcss/tailwind.css'
+import Link from 'next/link'
+import {Fragment} from 'react'
+
+export default function Home() {
+  return (
+
+
+<footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+    <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">   
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                KONTAKT
+            </div>
+              <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                doner@kebab.com
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                ul.Willowa 2, 43-309 Bielsko-Biała
+            </a>
+        </div>
+
+        
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Döner Kebab
+            </div>
+
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                Pomoc
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                Regulamin
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                Polityka prywatności
+            </a>
+        </div>
+
+        
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Płatności
+            </div>
+
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                VISA
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                PAY PAL
+            </a>
+            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 hover:underline">
+                Mastercard
+            </a>
+            
+        </div>
+
+        
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                SPOŁECZNOŚĆ
+            </div>
+              <a href="" button class="flex items-center pl-1 pb-1 rounded-full">
+                <img class="h-9 hover:bg-blue-600 duration-1000 rounded-full " src="Facebook.png"  alt="fb" />
+                <span class="ml-3 text-sm normal-case text-gray-300 font-medium">Facebook</span>
+              </a>  
+              <a href="" button class="flex items-center p-1 rounded-full ">
+                <img class="h-9 hover:bg-blue-400 duration-1000 rounded-full" src="Twitter.png" alt="tw" />
+                <span class="ml-3 text-sm normal-case text-gray-300 font-medium">Twitter</span>
+              </a>
+              <a href="" button class="flex items-center p-1 rounded-full">
+                <img class="h-9 hover:bg-pink-500 duration-1000 rounded-full" src="Instagram.png" alt="insta" />
+                <span class="ml-3 text-sm normal-case text-gray-300 font-medium">Instagram</span>
+              </a>
+        </div>
+    </div>
+
+    
+    <div class="pt-2">
+        <div class="flex pb-5 px-3 m-auto pt-5 
+            border-t border-gray-500 text-gray-400 text-sm 
+            flex-col md:flex-row max-w-6xl">
+            <div class="mt-2">
+                © Copyright 2021 Döner Kebab.
+            </div>
+        </div>
+    </div>
+</footer>
+    )
+}
