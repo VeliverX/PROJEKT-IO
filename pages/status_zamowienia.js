c12ef6c7433459adc49a0710655c4dd05d10ae1d
Jakub Pszczółka <jp057198@student.ath.edu.pl>
2022-01-16 17:02:14 +0100

    FrontEnd Statusu Zamówienia

---
+++ pages/status_zamowienia.js
@@ -0,0 +1,275 @@
+import styles from 'tailwindcss/tailwind.css'
+import Link from 'next/link'
+import {Fragment} from 'react'
+
+
+export default function Home() {
+    return (
+<body>
+        <header class="bg-yellow-400 text-white  hidden md:block sticky top-0 z-50 ">
+        <Fragment>
+    <div class="container mx-auto flex items-center h-24">
+      <Link href="strona_glowna">
+        <div>
+        <a href="" class="flex items-center justify-center">
+          <img class="h-14" src="Logo.png" alt="logo" />
+          <span class="ml-4 text-lg normal-case font-semibold text-white">Döner Kebab</span>
+        </a>
+        </div>
+        </Link>
+      <div class="contents font-semibold text-base lg:text-lg">
+        <ul class="ml-auto flex items-center">
+        <button class="
+        relative 
+        flex jutify-center items-center 
+        bg-transparent 
+        text-gray-600 rounded 
+        group">
+      
+      <span class="#">
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="44" 
+          height="44" 
+          fill="white"
+          class="bi bi-list" 
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </span>
+      <div class="
+        absolute top-full
+        hidden group-focus:block 
+        min-w-full w-max 
+        bg-white 
+        shadow-md mt-1 rounded
+        
+      ">
+        <ul class="text-left border rounded w-96">
+          <Link href="strona_glowna">
+          <li class="px-4 py-3 border-b hover:text-blue-500">
+            Strona Główna
+          </li>
+          </Link>
+          <Link href="menu">
+          <li class="px-4 py-3 border-b hover:text-blue-500">
+            Menu
+          </li>
+          </Link>
+          <Link href="koszyk">
+          <li class="px-4 py-3 border-b hover:text-blue-500">
+            Zamów Teraz
+          </li>
+          </Link>
+          <Link href="promocje">
+          <li class="px-4 py-3 border-b hover:text-blue-500">
+            Promocje
+          </li>
+          </Link>
+          <Link href="najchetniej_zamawiane">
+          <li class="px-4 py-3 border-b hover:text-blue-500">
+            Najchętniej zamawiane
+          </li>
+          </Link>
+          <Link href="login">
+          <li class="px-4 py-3 bg-yellow-200 font-semibold hover:text-blue-500">
+            Wyloguj
+          </li>
+          </Link>
+        </ul>
+      </div>
+    </button>
+        <div class="m-4">
+        <Link href="koszyk">
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="32" 
+          height="32" 
+          fill="currentColor" 
+          class="bi bi-cart3" 
+          viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+        </Link>
+        </div>
+        </ul>
+      </div>
+    </div>
+    </Fragment>
+    </header>
+<section>
+            <div class="bg-white p-8 rounded-md w-full">
+	<div class=" flex items-center justify-between pb-6">
+		<div>
+			<h2 class="text-gray-600 font-semibold">Status Zamówienia</h2>
+			<span class="text-xs">Wszystkie zamówienia</span>
+		</div>
+		</div>
+		<div>
+			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+					<table class="min-w-full leading-normal">
+						<thead>
+							<tr>
+								<th
+									class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Zamówienia
+								</th>
+								<th
+									class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Produkt
+								</th>
+								<th
+									class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Data
+								</th>
+								<th
+									class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Numer zamówienia
+								</th>
+								<th
+									class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Status
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<div class="flex items-center">
+											<div class="ml-3">
+												<p class="text-gray-900 whitespace-no-wrap">
+													1.
+												</p>
+											</div>
+										</div>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">Rollo Amerykańskie
+                                    <br/>
+                                    Kubełek
+                                    <br/>
+                                    Frytki
+                                    </p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+                                    21 Styczeń 2022
+									</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+										105
+									</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<span
+                                        class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+									<span class="relative">W trakcie przygotowania</span>
+									</span>
+								</td>
+							</tr>
+							<tr>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<div class="flex items-center">
+											<div class="ml-3">
+												<p class="text-gray-900 whitespace-no-wrap">
+													2.
+												</p>
+											</div>
+										</div>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">Kebab w bułce</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+										1 Styczeń 2022
+									</p>
+                                </td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+										77
+									</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<span
+                                        class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+									<span class="relative">Dostarczono</span>
+									</span>
+								</td>
+							</tr>
+							<tr>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<div class="flex items-center">
+											<div class="ml-3">
+												<p class="text-gray-900 whitespace-no-wrap">
+													3.
+												</p>
+											</div>
+										</div>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">Kubełek</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+										10 Grudzeń 2021
+									</p>
+                                </td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">
+										20
+									</p>
+								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<span
+                                        class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
+									<span class="relative">Wstrzymano</span>
+									</span>
+								</td>
+							</tr>
+							<tr>
+								<td class="px-5 py-5 bg-white text-sm">
+									<div class="flex items-center">
+											<div class="ml-3">
+												<p class="text-gray-900 whitespace-no-wrap">
+													4.
+												</p>
+											</div>
+										</div>
+								</td>
+								<td class="px-5 py-5 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">Kubełek</p>
+								</td>
+								<td class="px-5 py-5 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">3 Grudzeń 2021</p>
+								</td>
+								<td class="px-5 py-5 bg-white text-sm">
+									<p class="text-gray-900 whitespace-no-wrap">5</p>
+								</td>
+								<td class="px-5 py-5 bg-white text-sm">
+									<span
+                                        class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+									<span class="relative">Dostarczono</span>
+									</span>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+</body>
+    )
+}
