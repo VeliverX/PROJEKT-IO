076d4afb106c3a60d102ddff8fe79d9088860163
Jakub Pszczółka <jp057198@student.ath.edu.pl>
2022-01-30 16:35:48 +0100

    Zmiana kontaktu

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -4,33 +4,35 @@ import {Fragment} from 'react'
 
 export default function Home() {
   return (
-    <section class="flex justify-center items-center h-screen bg-gray-800">
+    <section class="flex justify-center items-center h-screen bg-yellow-400">
+    <title>Kontakt</title>
     <div class="max-w-md w-full bg-white rounded p-10  space-y-6">
-        <div class="#">
-            <p class="flex justify-center text-gray-600 text-3xl font-medium">Kontakt</p>
+        <div class="mb-4">
+            <p class="flex justify-center text-gray-600 text-3xl font-medium	">Kontakt</p>
         </div>
-        
-        <div 
-            class="text-xs uppercase text-gray-400 font-medium mb-6">MAIL
-            <a href="#" class="my-3 block text-black hover:text-gray-500 text-sm font-medium duration-700 hover:underline">doner@kebab.com</a>
-        </div>
-        
-
-        <div 
-            class="text-xs uppercase text-gray-400 font-medium mb-6">ADRES
-            <a href="#" class="my-3 block text-black hover:text-gray-500 text-sm font-medium duration-700 hover:underline">ul.Willowa 2, 43-309 Bielsko-Biała</a>
-        </div>
-
-        <div 
-            class="text-xs uppercase text-gray-400 font-medium mb-6">INFO   LINIA
-            <a href="#" class="my-3 block text-black hover:text-gray-500 text-sm font-medium duration-700 hover:underline">800-202-654</a>
-        </div>
-        
         <div>
-        <Link href="menu">
-        <a class="flex justify-center text-sm text-gray-500 hover:underline" href="#">Powrót</a>
+            <input class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="nazwa" placeholder="Nazwa"></input>
+        </div>
+        <div>
+            <input class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="email" placeholder="Email"></input>
+        </div>
+        <div>
+            <textarea 
+            id="wiadomosc" 
+            placeholder="Wiadomość" 
+            cols="30" 
+            rows="10"
+            class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"></textarea>
+        </div>
+        <Link href="strona_glowna">
+        <div>
+            <button class="w-full py-4 bg-yellow-400 bg-opacity-100 hover:bg-yellow-600 rounded text-sm font-bold text-gray-50 transition duration-200" type="submit">Wyslij</button>
+        </div>
         </Link>
-        </div> 
+        <div>
+        </div>  
+        <div class="flex items-center justify-between">
+        </div>
     </div>
 </section>
   )
